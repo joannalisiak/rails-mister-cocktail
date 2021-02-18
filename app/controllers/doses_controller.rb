8fb01cf7e14fb5728d2729d471318ebcf5bdda31
Joanna Lisiak <joanna.lisiak13@gmail.com>
2021-02-18 17:04:12 +0100

    Create all ingredients&doses views

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,12 +1,14 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, except: [:destroy]
+
   def new
     @dose = Dose.new
   end
 
   def create
-    @cocktail = cocktail.find(prarms[:cocktail_id])
-    @dose.cocktail = @cocktail
+    # raise
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -15,11 +17,23 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    set_dose
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:name)
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
   end
 end
