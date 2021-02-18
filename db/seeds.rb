02e307469de4cdc3ed3080f3c0cc8f61fd18283d
Joanna Lisiak <joanna.lisiak13@gmail.com>
2021-02-18 12:31:22 +0100

    Populate ingredients table with api

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'faker'
+require 'open-uri'
+require 'json'
+
+puts "Cleaning database..."
+Ingredient.destroy_all
+
+puts "Reading ingredient names..."
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+
+ingredients['drinks'].each do |i|
+  ingredient  = Ingredient.create!(
+    name: i['strIngredient1']
+  )
+  puts "Ingredient #{ingredient.name} succesfully created"
+end
+
+puts "All the ingredients successfully created!"
